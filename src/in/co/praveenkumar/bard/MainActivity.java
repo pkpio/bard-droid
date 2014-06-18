5fe1c07d2161142cc00bb56a0c4ddfa42eb822ca
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-18 05:33:33 +0530

    Initialized Android project
    Just a Helloworld project right now.

---
+++ src/in/co/praveenkumar/bard/MainActivity.java
@@ -0,0 +1,13 @@
+package in.co.praveenkumar.bard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
