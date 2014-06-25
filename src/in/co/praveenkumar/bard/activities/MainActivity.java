228e1de8837815408b84e498daa37f4c17c61d4d
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-06-25 07:13:53 +0530

    MainActivity to Activities package

---
+++ src/in/co/praveenkumar/bard/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package in.co.praveenkumar.bard.activities;
+
+import in.co.praveenkumar.bard.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
