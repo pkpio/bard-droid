ee56b29c7453e387aaaaa42db61d0b623585ae41
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-07-24 21:41:58 +0530

    Cleanup of code for Hexdump in ADKReader

--- src/in/co/praveenkumar/bard/io/ADKReader.java
+++ src/in/co/praveenkumar/bard/io/ADKReader.java
@@ -124,9 +124,6 @@ public class ADKReader {
 					e.printStackTrace();
 					break;
 				}
-				// read = bytesToHex(buffer);// Arrays.toString(buffer);
-
-				// publishProgress(0);
 			}
 
 			return null;
@@ -142,18 +139,6 @@ public class ADKReader {
 			uu.reInitAccessory();
 		}
 
-	}
-
-	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
-	public static String bytesToHex(byte[] bytes) {
-		char[] hexChars = new char[bytes.length * 2];
-		for (int j = 0; j < bytes.length; j++) {
-			int v = bytes[j] & 0xFF;
-			hexChars[j * 2] = hexArray[v >>> 4];
-			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-		}
-		return new String(hexChars);
 	}
 
 }
