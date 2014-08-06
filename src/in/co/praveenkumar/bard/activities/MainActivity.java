f4bb361d947f54f508eb1c9b6d7dcae201a2c8e7
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-06 09:03:59 +0530

    Fixed error with full resolution due to ByteBuffer pos.
    POsition set to 0 before pixel copy

--- src/in/co/praveenkumar/bard/activities/MainActivity.java
+++ src/in/co/praveenkumar/bard/activities/MainActivity.java
@@ -108,6 +108,7 @@ public class MainActivity extends Activity {
 	public void setupImage(ByteBuffer buffer) {
 		System.out.println("setUpImage called");
 		Bitmap bitmap;
+		buffer.position(0);
 		bitmap = Bitmap.createBitmap(1024, 768, Bitmap.Config.RGB_565);
 		bitmap.copyPixelsFromBuffer(buffer);
 		buffer.rewind();
