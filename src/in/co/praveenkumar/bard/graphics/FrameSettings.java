32753e394fb4f7fed0e5ec9f8728bac3b3d4267d
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-07 09:17:24 +0530

    Added FrameSettings class

---
+++ src/in/co/praveenkumar/bard/graphics/FrameSettings.java
@@ -0,0 +1,9 @@
+package in.co.praveenkumar.bard.graphics;
+
+public class FrameSettings {
+	public static final int WIDTH = 1024;
+	public static final int HEIGHT = 768;
+	public static final int BPP = 2; // Bytes per pixel
+	public static final int FPS = 20;
+
+}
