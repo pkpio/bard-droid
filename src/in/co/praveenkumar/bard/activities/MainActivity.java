fee231f5cae962a2ec9769daeffe89bfeda74b2e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-08 12:38:35 +0530

    Testing with totally new USBReader code from moverbot

--- src/in/co/praveenkumar/bard/activities/MainActivity.java
+++ src/in/co/praveenkumar/bard/activities/MainActivity.java
@@ -3,6 +3,7 @@ package in.co.praveenkumar.bard.activities;
 import in.co.praveenkumar.bard.R;
 import in.co.praveenkumar.bard.graphics.Frame;
 import in.co.praveenkumar.bard.io.ADKReader;
+import in.co.praveenkumar.bard.io.USBControl;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
@@ -30,224 +31,66 @@ public class MainActivity extends Activity {
 	final String IDENT_MANUFACTURER = "BeagleBone";
 	final String USB_PERMISSION = "in.co.praveenkumar.bard.activities.MainActivity.USBPERMISSION";
 
-	UsbAccessory mAccessory = null;
-	public static FileInputStream mFin = null;
-	public static FileDescriptor mFd = null;
-	ADKReader mADKReader;
+	// Handler, Threads
+	private Handler UIHandler = new Handler();
+	private USBControlServer usbConnection;
 
-	PendingIntent mPermissionIntent = null;
-
-	TextView accessoryStatus;
-	ImageView remoteScreen;
-
-	Frame fp = new Frame(); // Instantiate frame details
-
+	// Activity Lifecycle
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Initialize widgets
-		accessoryStatus = (TextView) findViewById(R.id.main_accessory_status);
-		remoteScreen = (ImageView) findViewById(R.id.remote_screen);
+		setupUSB();
 
-		// Register receiver for actions
-		IntentFilter i = new IntentFilter();
-		i.addAction(UsbManager.ACTION_USB_ACCESSORY_ATTACHED);
-		i.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-		i.addAction(USB_PERMISSION);
-		registerReceiver(mUsbReceiver, i);
-
-		if (getIntent().getAction().equals(
-				"android.hardware.usb.action.USB_ACCESSORY_ATTACHED"))
-			openAccessory(getIntent());
-		else
-			requestPermission();
-
-		// Start Frame updater thread
-		frameUpdate();
 	}
 
-	// Temporary listener
-	public void sndBtnClick(View v) {
-		updateImage(Frame.frameBuffer);
+	private void setupUSB() {
+
+		System.out.println("Starting USB...");
+		usbConnection = new USBControlServer(UIHandler);
+		System.out.println("Done\n");
 	}
 
-	public void updateImage(ByteBuffer buffer) {
-		System.out.println("setUpImage called");
-		Bitmap bitmap;
-		buffer.position(0);
-		bitmap = Bitmap.createBitmap(1024, 768, Bitmap.Config.RGB_565);
+	public class USBControlServer extends USBControl {
 
-		/*
-		 * -TODO- Some strange thing here. Sometimes copyPixelsFromBuffer is
-		 * reading outside the buffer range
-		 */
-		try {
-			bitmap.copyPixelsFromBuffer(buffer);
-			buffer.rewind();
-			remoteScreen.setImageBitmap(bitmap);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Exception with copyPixelsFromBuffer");
+		public USBControlServer(Handler ui) {
+			super(getApplicationContext(), ui);
 		}
-	}
 
-	@Override
-	protected void onDestroy() {
-		unregisterReceiver(mUsbReceiver);
-		super.onDestroy();
-	}
-
-	private void openAccessory(Intent i) {
-		Log.d(DEBUG_TAG, "Accessory attached");
-		UsbAccessory accessory = UsbManager.getAccessory(i);
-		mAccessory = accessory;
-		initAccessory(mAccessory);
-	}
-
-	private void initAccessory(UsbAccessory accessory) {
-		FileDescriptor fd = null;
-		try {
-			fd = UsbManager.getInstance(this).openAccessory(accessory)
-					.getFileDescriptor();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			finish();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-			finish();
-		}
-		mFin = new FileInputStream(fd);
-		mADKReader = new ADKReader(mFin, new UIUpdater(), this, accessory);
-
-		// Start to monitor incoming data
-		mADKReader.start();
-	}
-
-	private void closeAccessory(Intent i) {
-		UsbAccessory accessory = UsbManager.getAccessory(i);
-		if (accessory != null && accessory.equals(mAccessory)) {
-			mAccessory = null;
-		}
-	}
-
-	private void requestPermission() {
-		Log.d(DEBUG_TAG, "Requesting permission");
-
-		UsbAccessory[] accessories = UsbManager.getInstance(this)
-				.getAccessoryList();
-
-		if (accessories != null) {
-			for (UsbAccessory a : accessories) {
-				Log.d(DEBUG_TAG, "accessory: " + a.getManufacturer());
-				if (a.getManufacturer().equals(IDENT_MANUFACTURER)) {
-					mPermissionIntent = PendingIntent.getBroadcast(this, 0,
-							new Intent(USB_PERMISSION), 0);
-					UsbManager.getInstance(this).requestPermission(a,
-							mPermissionIntent);
-					Log.d(DEBUG_TAG, "permission requested");
-					break;
-				}
-			}
-		} else {
-			Log.d(DEBUG_TAG, "No accessories");
-		}
-	}
-
-	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
 		@Override
-		public void onReceive(Context context, Intent intent) {
-			Log.d(DEBUG_TAG, "Broadcast received");
-			String action = intent.getAction();
+		public void onReceive(byte[] msg) {
 
-			// Attached
-			if (UsbManager.ACTION_USB_ACCESSORY_ATTACHED.equals(action)) {
-				if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED,
-						false))
-					openAccessory(intent);
-				else
-					Log.d(DEBUG_TAG, "permission denied for accessory");
-			}
-
-			// Detached
-			else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-				closeAccessory(intent);
-			}
-
-			// Permission answered
-			else if (USB_PERMISSION.equals(action)) {
-				Log.d(DEBUG_TAG, "Permission answered");
-
-				// Permission granted
-				if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED,
-						false)) {
-					UsbAccessory[] accessories = UsbManager.getInstance(
-							getApplicationContext()).getAccessoryList();
-					for (UsbAccessory a : accessories) {
-						Log.d(DEBUG_TAG, "accessory: " + a.getManufacturer());
-						if (a.getManufacturer().equals("BeagleBone")) {
-							mAccessory = a;
-							initAccessory(mAccessory);
-						}
-					}
-				}
-
-				// permission not granted
-				else {
-					// TO-DO
-				}
-
+			int i = 0;
+			System.out.println("Received onReceive");
+			int pageIndex = (int) (msg[0] & 0x0000000ff)
+					+ (int) (msg[1] << 8 & 0x0000ff00);
+			
+			// Update frame data
+			int framePos = pageIndex * 4096;
+			if ((framePos - (msg.length - 2)) <= Frame.FRAME_LENGTH) {
+				Frame.frameBuffer.position(framePos);
+				Frame.frameBuffer.put(msg, 2, msg.length - 2);
 			}
 
 		}
-	};
 
-	public class UIUpdater {
-
-		public void reInitAccessory() {
-			resetInputStream();
-		}
-
-		public void restartReaderThread() {
-			initAccessory(mAccessory);
-		}
-
-		public void updateFrame() {
-			updateImage(Frame.frameBuffer);
-		}
-
-	}
-
-	private void frameUpdate() {
-		updateImage(Frame.frameBuffer);
-
-		// Wait before doing next frame update
-		Handler myHandler = new Handler();
-		myHandler.postDelayed(frameUpdater, 250);
-	}
-
-	private Runnable frameUpdater = new Runnable() {
 		@Override
-		public void run() {
-			frameUpdate();
+		public void onNotify(String msg) {
+			// console(msg);
 		}
-	};
 
-	public void resetInputStream() {
-		FileDescriptor fd = null;
-		try {
-			fd = UsbManager.getInstance(this).openAccessory(mAccessory)
-					.getFileDescriptor();
-			mFd = fd;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			finish();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
+		@Override
+		public void onConnected() {
+			// usb.enable();
+		}
+
+		@Override
+		public void onDisconnected() {
+			// usb.pause();
 			finish();
 		}
-		mFin = new FileInputStream(fd);
+
 	}
 
 }
--- src/in/co/praveenkumar/bard/io/ADKReader.java
+++ src/in/co/praveenkumar/bard/io/ADKReader.java
@@ -1,15 +1,8 @@
 package in.co.praveenkumar.bard.io;
 
-import in.co.praveenkumar.bard.activities.MainActivity;
-import in.co.praveenkumar.bard.activities.MainActivity.UIUpdater;
-import in.co.praveenkumar.bard.graphics.Frame;
-
 import java.io.FileInputStream;
-import java.io.IOException;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
 
 import com.android.future.usb.UsbAccessory;
 
@@ -17,80 +10,80 @@ public class ADKReader {
 	final String DEBUG_TAG = "BARD.IO.ADKReader";
 
 	FileInputStream mFin = null;
-	UIUpdater uu = null;
+	//UIUpdater uu = null;
 	Context context = null;
 	UsbAccessory mAccessory = null;
 
-	public ADKReader(FileInputStream mFin, UIUpdater uu, Context context,
-			UsbAccessory mAccessory) {
-		this.mFin = mFin;
-		this.uu = uu;
-		this.context = context;
-		this.mAccessory = mAccessory;
-	}
-
-	public void start() {
-		new dataListener().execute(0);
-	}
-
-	public void setFinputstream(FileInputStream mFin) {
-		this.mFin = mFin;
-	}
-
-	private class dataListener extends AsyncTask<Integer, Integer, Long> {
-
-		@Override
-		protected void onProgressUpdate(Integer... progress) {
-			uu.updateFrame();
-		}
-
-		@Override
-		protected Long doInBackground(Integer... params) {
-
-			Log.d(DEBUG_TAG, "ADKReader doInbackground called");
-
-			while (true) { // read data
-				byte[] buffer = new byte[4098];
-				try {
-					Frame.bytesReceived += mFin.read(buffer);
-
-					// Get the index of the page
-					int pageIndex = (int) (buffer[0] & 0x0000000ff)
-							+ (int) (buffer[1] << 8 & 0x0000ff00);
-
-					System.out.println("Page index: " + pageIndex);
-
-					int framePos = pageIndex * 4096;
-					if ((framePos - (buffer.length - 2)) <= Frame.FRAME_LENGTH) {
-						Frame.frameBuffer.position(framePos);
-						Frame.frameBuffer.put(buffer, 2, buffer.length - 2);
-					}
-
-				} catch (IOException e) {
-					Log.d(DEBUG_TAG, "Caught a Reader exception");
-					e.printStackTrace();
-					uu.reInitAccessory();
-					mFin = MainActivity.mFin;
-				} catch (Exception e) {
-					Log.d(DEBUG_TAG,
-							"Unknow exception while getting inputstream");
-					e.printStackTrace();
-					uu.reInitAccessory();
-					mFin = MainActivity.mFin;
-				}
-			}
-
-		}
-
-		@Override
-		protected void onPostExecute(Long result) {
-			// We reached here means our read loop exited.
-			// Most common reason is BAD File Descriptor.
-			// So, open accessory again with updated FD.
-			Log.d(DEBUG_TAG, "ADKReader Post execute called");
-			uu.restartReaderThread();
-		}
-
-	}
+	// public ADKReader(FileInputStream mFin, UIUpdater uu, Context context,
+	// UsbAccessory mAccessory) {
+	// this.mFin = mFin;
+	// this.uu = uu;
+	// this.context = context;
+	// this.mAccessory = mAccessory;
+	// }
+	//
+	// public void start() {
+	// new dataListener().execute(0);
+	// }
+	//
+	// public void setFinputstream(FileInputStream mFin) {
+	// this.mFin = mFin;
+	// }
+	//
+	// private class dataListener extends AsyncTask<Integer, Integer, Long> {
+	//
+	// @Override
+	// protected void onProgressUpdate(Integer... progress) {
+	// uu.updateFrame();
+	// }
+	//
+	// @Override
+	// protected Long doInBackground(Integer... params) {
+	//
+	// Log.d(DEBUG_TAG, "ADKReader doInbackground called");
+	//
+	// while (true) { // read data
+	// byte[] buffer = new byte[4098];
+	// try {
+	// Frame.bytesReceived += mFin.read(buffer);
+	//
+	// // Get the index of the page
+	// int pageIndex = (int) (buffer[0] & 0x0000000ff)
+	// + (int) (buffer[1] << 8 & 0x0000ff00);
+	//
+	// System.out.println("Page index: " + pageIndex);
+	//
+	// int framePos = pageIndex * 4096;
+	// if ((framePos - (buffer.length - 2)) <= Frame.FRAME_LENGTH) {
+	// Frame.frameBuffer.position(framePos);
+	// Frame.frameBuffer.put(buffer, 2, buffer.length - 2);
+	// }
+	//
+	// } catch (IOException e) {
+	// Log.d(DEBUG_TAG, "Caught a Reader exception");
+	// e.printStackTrace();
+	// uu.reInitAccessory();
+	// mFin = MainActivity.mFin;
+	// } catch (Exception e) {
+	// Log.d(DEBUG_TAG,
+	// "Unknow exception while getting inputstream");
+	// e.printStackTrace();
+	// uu.reInitAccessory();
+	// mFin = MainActivity.mFin;
+	// }
+	// }
+	//
+	// }
+	//
+	// @Override
+	// protected void onPostExecute(Long result) {
+	// // We reached here means our read loop exited.
+	// // Most common reason is BAD File Descriptor.
+	// // So, open accessory again with updated FD.
+	// Log.d(DEBUG_TAG, "ADKReader Post execute called");
+	// uu.restartReaderThread();
+	// }
+	//
+	// }
 
 }
