f3255bde2fb6896c836aeacb35cbb29b1a33e3dc
Praveen Kumar Pendyala <praveendath92@gmail.com>
2014-08-12 18:37:55 +0530

    Documentation in FrameSettings

--- src/in/co/praveenkumar/bard/graphics/FrameSettings.java
+++ src/in/co/praveenkumar/bard/graphics/FrameSettings.java
@@ -1,9 +1,34 @@
 package in.co.praveenkumar.bard.graphics;
 
 public class FrameSettings {
+
+	/**
+	 * Width of the frame
+	 */
 	public static final int WIDTH = 1024;
+
+	/**
+	 * Height of the frame
+	 */
 	public static final int HEIGHT = 768;
-	public static final int BPP = 2; // Bytes per pixel
+
+	/**
+	 * Bytes per pixel
+	 */
+	public static final int BPP = 2;
+
+	/**
+	 * Frames per second.
+	 */
 	public static final int FPS = 20;
+
+	/**
+	 * Bytes per compression. 
+	 * <br/>
+	 * This is the number of actual bytes that are
+	 * encoded into each line of rle compression. This is equal to the number of
+	 * bytes in one page on framebuffer. i.e., 4096.
+	 */
+	public static final int BPC = WIDTH * BPP * 2;
 
 }
